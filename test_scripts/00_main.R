dfbc62a8cfb35feec4c9eb39174b6fe710a5cd6b
ivanhanigan <ivan.hanigan@gmail.com>
2021-07-28 17:47:40 +1000

    add main R script to run the workflow. AirFlow should do this job instead.

---
+++ test_scripts/00_main.R
@@ -0,0 +1,7 @@
+# 00_main.R
+# aim: a simple data input example
+# ivanhanigan
+
+source("test_scripts/01_load_air_pollution.R")
+source("test_scripts/02_load_spatial_boundaries.R")
+source("test_scripts/03_merge_air_pollution_with_boundaries.R")
