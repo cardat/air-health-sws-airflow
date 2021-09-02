f3fcea9a2df433e47e6406ed787ac195c5e9d549
Sergio <sergio.pintaldi@gmail.com>
2021-09-02 13:59:12 +1000

    feat: added install script and R docker file

---
+++ test_scripts/install.R
@@ -0,0 +1,24 @@
+packages <- c(
+  "tidyverse",
+  "dplyr",
+  "here",
+  "rgdal",
+  "raster",
+  "cloudstoR",
+  "purr"
+)
+
+installed <- installed.packages()[, "Package"]
+old_pkgs <- old.packages()[, "Package"]
+
+for (package in packages) {
+  if (package %in% installed && package %in% old_pkgs) {
+    cat("Updating", package, "...\n\n")
+    install.packages(package)
+  } else if (!package %in% installed) {
+    cat("Installing", package, "...\n\n")
+    install.packages(package)
+  } else {
+    cat("Nothing to update or install for", package, "\n")
+  }
+}
