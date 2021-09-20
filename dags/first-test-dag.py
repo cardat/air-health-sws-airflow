46d7b933ae4fbe76972999897669828d94708a77
Rosemarie Sadsad <rosies@gmail.com>
2021-09-20 17:36:16 +1000

    Update first-test-dag.py

--- dags/first-test-dag.py
+++ dags/first-test-dag.py
@@ -4,7 +4,6 @@ from airflow.utils.dates import days_ago
 
 from datetime import timedelta
 
-
 def_args = {
     'owner': 'sergio',
     'depends_on_past': False,
@@ -18,7 +17,7 @@ def_args = {
 with DAG(
     'first_test_dag',
     default_args=def_args,
-    description='first "Hello word" in TERNA Airflow',
+    description='first "Hello word" in TERN Airflow',
     schedule_interval=None,
     start_date=days_ago(2),
     tags=['first', 'test']
@@ -26,4 +25,6 @@ with DAG(
     t1 = BashOperator(
         task_id='print_hello_sergio',
         bash_command=f'echo "Ciao {def_args["owner"]}! How is it going?"',
+        bash_command=f'echo "{{ var.value.home }}"',
+        
     )
