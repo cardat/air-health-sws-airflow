998354a7240d239bd41ffac29dbb92b534f14985
Rosemarie Sadsad <rosies@gmail.com>
2021-10-07 12:02:23 +1100

    Update k8s_pod_RS.py

--- dags/k8s_pod_RS.py
+++ dags/k8s_pod_RS.py
@@ -10,7 +10,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=1),
 }
 
 namespace = conf.get('kubernetes', 'NAMESPACE')
