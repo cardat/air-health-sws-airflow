40125e5d4b2ed56233b255f718bd9ac7f16ed4d5
Rosemarie Sadsad <rosies@gmail.com>
2021-10-07 12:16:39 +1100

    Update k8s_pod_RS.py

--- dags/k8s_pod_RS.py
+++ dags/k8s_pod_RS.py
@@ -16,12 +16,12 @@ namespace = conf.get('kubernetes', 'NAMESPACE')
 
 # This will detect the default namespace locally and read the
 # environment namespace when deployed to Astronomer.
-if namespace =='default':
-    config_file = '/usr/local/airflow/include/.kube/config'
-    in_cluster=False
-else:
-    in_cluster=True
-    config_file=None
+#if namespace =='default':
+#    config_file = '/usr/local/airflow/include/.kube/config'
+#    in_cluster=False
+#else:
+#    in_cluster=True
+#    config_file=None
 
 dag = DAG('k8s_pod_RS',
           schedule_interval='@once',
@@ -36,13 +36,15 @@ compute_resources = \
 
 with dag:
     k = KubernetesPodOperator(
-        namespace=namespace,
+#        namespace=namespace,
+        namespace="airflow-car",
         image="hello-world",
         labels={"foo": "bar"},
         name="airflow-test-pod",
         task_id="task-one",
         startup_timeout_seconds=300,
-        in_cluster=in_cluster, # if set to true, will look in the cluster, if false, looks for file
+        #in_cluster=in_cluster, # if set to true, will look in the cluster, if false, looks for file
+        in_cluster=True, # if set to true, will look in the cluster, if false, looks for file
         cluster_context='docker-for-desktop', # is ignored when in_cluster is set to True
         config_file=config_file,
         resources=compute_resources,
