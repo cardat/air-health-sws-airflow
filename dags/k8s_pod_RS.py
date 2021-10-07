54c6ff636d095b8a25adde2093bdb0f9639b3b97
Rosemarie Sadsad <rosies@gmail.com>
2021-10-07 16:40:23 +1100

    Update k8s_pod_RS.py

--- dags/k8s_pod_RS.py
+++ dags/k8s_pod_RS.py
@@ -8,18 +8,9 @@ default_args = {
     'start_date': datetime(2021, 10, 7)
 }
 
-#namespace = conf.get('kubernetes', 'NAMESPACE')
-
 dag = DAG('k8s_pod_RS',
           default_args=default_args
 )
-
-# This is where we define our desired resources.
-compute_resources = \
-  {'request_cpu': '100m',
-  'request_memory': '4Mi',
-  'limit_cpu': '100m',
-  'limit_memory': '4Mi'}
 
 with dag:
     k = KubernetesPodOperator(
@@ -27,7 +18,5 @@ with dag:
         task_id="task-one",
         namespace="airflow-car",
         image="hello-world",
-#        startup_timeout_seconds=300,
-#        resources=compute_resources,
         get_logs=True
     )
