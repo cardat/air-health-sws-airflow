0e548f03ac191e4336ca244ade3ff17462fc7819
Sergio <sergio.pintaldi@gmail.com>
2021-10-13 11:48:19 +1100

    feat: remove sending email

--- dags/k8_pod_example_gerhard.py
+++ dags/k8_pod_example_gerhard.py
@@ -12,7 +12,7 @@ default_args = {
     'owner': 'sergio',
     'depends_on_past': False,
     'email': ['sergio.pintaldi@sydney.edu.au'],
-    'email_on_failure': True,
+    'email_on_failure': False,
     'email_on_retry': False,
     'retries': 0,
     'retry_delay': timedelta(minutes=1)
