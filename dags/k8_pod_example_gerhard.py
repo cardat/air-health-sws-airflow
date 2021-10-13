a964e593623e9f9138ee709b7b0c1e1ef92ce165
Sergio <sergio.pintaldi@gmail.com>
2021-10-13 11:40:06 +1100

    fix: update as per online docs

--- dags/k8_pod_example_gerhard.py
+++ dags/k8_pod_example_gerhard.py
@@ -32,21 +32,24 @@ with DAG(
         # The ID specified for the task.
         task_id="pod_hello_task",
         is_delete_operator_pod=True,
-        get_logs=False,
+        get_logs=True,
+        # labels={"foo": "bar"},
+        in_cluster=True,
         # Name of task you want to run, used to generate Pod ID.
         name="pod_hello_task",
         #########################################################
         # k8s pod config below
         #########################################################
         # cmds is optional ... depends on container to run
-        cmds=["bash"],
+        cmds=["bash", "-cx"],
         # cmd arguments
-        arguments=["echo 10"],
+        arguments=["echo", "10", "echo pwd"],
         # not 100% sure whether namespace is required, but should be airflow-car
         namespace="airflow-car",
         image="alpine:3.10",
         image_pull_policy="IfNotPresent",
-        image_pull_secrets=["sergio-dockerhub-credentials"],
+        # image_pull_secrets=["sergio-dockerhub-credentials"],
+        image_pull_secrets=[k8s.V1LocalObjectReference('sergio-dockerhub-credentials')],
         # it's always good to specify minimum resource requirements (this helps the k8s scheduler)
         resources=k8s.V1ResourceRequirements(
             requests={"cpu": "0.1"},
