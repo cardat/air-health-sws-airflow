9c2d342cd4c4ecb888c053e66390f720a2a5cfa6
Sergio <sergio.pintaldi@gmail.com>
2021-10-15 10:20:23 +1100

    fix: remove assignment error

--- dags/k8s_pod_simple_pipeline.py
+++ dags/k8s_pod_simple_pipeline.py
@@ -17,14 +17,13 @@ default_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
-
 # volume = k8s.V1Volume(empty_dir={}, name="airflow-dags")
 secret_volume = k8s.V1Volume(
     name="github-secret",
     secret=k8s.V1LocalObjectReference('gitsync-ssh')
 )
 
-secret_vol_mount = vol_mount = k8s.V1VolumeMount(
+secret_vol_mount = k8s.V1VolumeMount(
     name="github-secret",
     mount_path="/etc/secret-volume",
     read_only=True
