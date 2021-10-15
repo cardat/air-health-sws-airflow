5465d0e6a076f5028a04b435687a9c0d18834596
Sergio <sergio.pintaldi@gmail.com>
2021-10-15 11:24:10 +1100

    fix: change class name of volume secret
    Signed-off-by: Sergio <sergio.pintaldi@gmail.com>

--- dags/k8s_pod_simple_pipeline.py
+++ dags/k8s_pod_simple_pipeline.py
@@ -20,7 +20,8 @@ default_args = {
 # volume = k8s.V1Volume(empty_dir={}, name="airflow-dags")
 secret_volume = k8s.V1Volume(
     name="github-secret",
-    secret=k8s.V1LocalObjectReference('gitsync-ssh')
+    # secret=k8s.V1LocalObjectReference('gitsync-ssh')
+    secret=k8s.V1SecretVolumeSource(secret_name='gitsync-ssh')
 )
 
 secret_vol_mount = k8s.V1VolumeMount(
