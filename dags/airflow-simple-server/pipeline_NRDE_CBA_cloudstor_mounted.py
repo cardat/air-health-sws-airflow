472063678e42bbbfd3c378170cacd590c978e960
Sergio <sergio.pintaldi@gmail.com>
2021-12-14 13:24:09 +1100

    feat: add workflow with mounted cloudstor

---
+++ dags/airflow-simple-server/pipeline_NRDE_CBA_cloudstor_mounted.py
@@ -0,0 +1,44 @@
+import os
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+from airflow.models import Variable
+
+from datetime import timedelta
+
+
+def_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['admin@myemail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=1),
+}
+
+with DAG(
+    'pipeline_NRDE_CBA',
+    default_args=def_args,
+    description='Run the NRDE CBA pipeline given a file location with the code',
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=['data', 'R', 'pipeline', 'NRDE'],
+) as dag:
+
+    PIPELINE_PATH = "/mnt/data/repos/air-health-sws-airflow/NRDE_CBA_scripts/NRDE_CBA_main_mounted_cloudstor.R"
+    DATA_ROOT = "/cloudstor/Shared/"
+
+    task = BashOperator(
+        task_id="Running_NRDE_CBA_main",
+        bash_command="""
+            set -euo pipefail;
+            Rscript "{{ params.path }}" -d "{{ params.data_path }}" -o "/cloudstor/NRDE_CBA_outs"
+        """,
+        params={
+            'path': os.path.basename(PIPELINE_PATH),
+            'data_path': DATA_ROOT,
+        },
+        cwd=os.path.dirname(PIPELINE_PATH),
+    )
